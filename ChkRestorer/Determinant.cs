0f60205cad9040270e80706042dea88d70978a00
Yan Dekov <StewieSolden147@gmail.com>
2024-06-16 06:36:27 +0700

    minor change, no upd version

--- ChkRestorer/Determinant.cs
+++ ChkRestorer/Determinant.cs
@@ -7,46 +7,32 @@ namespace ChkRestorer
     {
         private static byte[] GetSourceSignature<T>(T src, int size = 32) where T : notnull
         {
-            try
-            {
-                var fileHeader = new byte[size];
+            var fileHeader = new byte[size];
 
-                if (src is Stream stream)
-                {
-                    stream.Read(fileHeader, 0, fileHeader.Length);
-                    return fileHeader;
-                }
-                if (src is string filePath)
-                {
-                    using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-                    fs.Read(fileHeader, 0, fileHeader.Length);
-                    return fileHeader;
-                }
-
-                throw new NotSupportedException("Not supported generic type");
-            }
-            catch (Exception)
+            if (src is Stream stream)
             {
-                throw;
+                stream.Read(fileHeader, 0, fileHeader.Length);
+                return fileHeader;
             }
+            if (src is string filePath)
+            {
+                using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                fs.Read(fileHeader, 0, fileHeader.Length);
+                return fileHeader;
+            }
+
+            throw new NotSupportedException("Not supported generic type");
         }
 
         private static void ChangeExtension(string filePath, string targetPath, string extension)
         {
-            try
-            {
-                var fileInfo = new FileInfo(filePath);
-                var newFileName = $"{Path.GetFileNameWithoutExtension(fileInfo.Name)}{extension}";
+            var fileInfo = new FileInfo(filePath);
+            var newFileName = $"{Path.GetFileNameWithoutExtension(fileInfo.Name)}{extension}";
 
-                if (!Directory.Exists(targetPath))
-                    Directory.CreateDirectory(targetPath);
+            if (!Directory.Exists(targetPath))
+                Directory.CreateDirectory(targetPath);
 
-                File.Move(filePath, Path.Join(targetPath, newFileName));
-            }
-            catch (FileNotFoundException)
-            {
-                throw;
-            }
+            File.Move(filePath, Path.Join(targetPath, newFileName));
         }
 
         private static string? CompareExtension(byte[] header)
@@ -58,7 +44,7 @@ namespace ChkRestorer
                     bool match = true;
                     for (int i = 0; i < signature.Length; i++)
                     {
-                        if (!header[i].Equals(signature[i]))
+                        if (header[i] != signature[i])
                         {
                             match = false;
                             break;
@@ -76,64 +62,38 @@ namespace ChkRestorer
 
         public static void ChangeSourceExtensionRange(string folderPath, string targetPath, bool autoCreate = true)
         {
-            try
+            if (autoCreate && !Directory.Exists(targetPath))
+                Directory.CreateDirectory(targetPath);
+
+            var files = Directory.GetFiles(folderPath, "*.CHK", SearchOption.TopDirectoryOnly);
+            if (files.Length == 0)
+                throw new FileNotFoundException($"No .CHK files found in {folderPath}");
+
+            foreach (var filePath in files)
             {
-                if (autoCreate)
-                    if (!Directory.Exists(targetPath))
-                        Directory.CreateDirectory(targetPath);
-
-                var files = Directory.GetFiles(folderPath, "*.CHK", SearchOption.TopDirectoryOnly);
-                if (files is null || files.Length <= 0)
-                    throw new FileNotFoundException($"No one .CHK file was not found in {folderPath}");
-
-                foreach (var filePath in files)
+                var header = GetSourceSignature(filePath);
+                var extension = CompareExtension(header);
+                if (extension != null)
                 {
-                    var header = GetSourceSignature(filePath);
-                    var extension = CompareExtension(header);
-                    if (extension is null)
-                        continue;
-
                     ChangeExtension(filePath, targetPath, extension);
                 }
-            }
-            catch (DirectoryNotFoundException)
-            {
-                throw;
-            }
-            catch (FileNotFoundException)
-            {
-                throw;
             }
         }
 
         public static void ChangeSourceExtension(string filePath, string targetPath)
         {
-            try
-            {
-                var header = GetSourceSignature(filePath);
-                var extension = CompareExtension(header) ?? throw new NotSupportedException("Could not match file extension");
+            var header = GetSourceSignature(filePath);
+            var extension = CompareExtension(header) ?? throw new NotSupportedException("Could not match file extension");
 
-                ChangeExtension(filePath, targetPath, extension);
-            }
-            catch (FileNotFoundException)
-            {
-                throw;
-            }
+            ChangeExtension(filePath, targetPath, extension);
         }
 
         public static string GetExtension<T>(T src) where T : notnull
         {
-            try
-            {
-                var header = GetSourceSignature(src);
-                var extension = CompareExtension(header) ?? throw new NotSupportedException("Could not match file extension");
+            var header = GetSourceSignature(src);
+            var extension = CompareExtension(header) ?? throw new NotSupportedException("Could not match file extension");
 
-                return extension;
-            }
-            catch (FileNotFoundException)
-            {
-                throw;
-            }
+            return extension;
         }
     }
 }
